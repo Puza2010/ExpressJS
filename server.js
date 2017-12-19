d87ae631b994de661914d603d52f10a0a9e0356f
Przemek <puza147@gmail.com>
2017-12-19 11:32:13 +0100

    17_4

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-var fs = require('fs');
 var express = require('express');
 var app = express();
-var stringifyFile;
-var bodyParser = require('body-parser');
+app.use(express.static('assets'));
 
-app.use(bodyParser.json());
-
-app.get('/getNote', function (req, res) {
-	fs.readFile('./test.json', 'utf8', function(err, data) {
-		if (err) throw err;
-		stringifyFile = data
-		res.send(data);
-	});
+app.get('/', function (req, res) {
+    res.sendFile('/index.html');
 });
 
-app.post('/updateNote/:note', function (req, res) {
-    res.send(stringifyFile + req.params.note);
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-		if (err) throw err;
-		console.log('file updated');
-	});
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
-app.listen(3000);
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
