6e49c8f4e5506c4077ce69a842e86f45192a139b
Przemek <puza147@gmail.com>
2017-12-21 11:04:17 +0100

    17_7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '731844780073-c7iq8i5onsvipbehloum7qgm6l6b3885.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'ZIU4DfKtGl6wx9xEwaqI2x0r',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
